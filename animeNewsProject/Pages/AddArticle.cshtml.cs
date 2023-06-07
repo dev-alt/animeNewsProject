6ce466ecfc78a0256c1dd454f71e4d9a5c06d3be
Andrew <andrew.logan@hotmail.com>
2023-06-07 16:04:46 +1200

    v2

---
+++ animeNewsProject/Pages/AddArticle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace animeNewsProject.Pages
+{
+    public class AddArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
